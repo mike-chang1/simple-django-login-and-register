b2df3c9d7aa6938c2b3f93e927fa236f330a8585
Mike Chang <mike.chang@payjoy.com>
2021-11-03 09:07:46 -0700

    Add allowed hosts for ElasticBeanstalk

--- source/app/settings.py
+++ source/app/settings.py
@@ -6,3 +6,5 @@ if IS_PRODUCTION:
     from .conf.production.settings import *
 else:
     from .conf.development.settings import *
+
+ALLOWED_HOSTS = ['eb-django-app-dev.elasticbeanstalk.com','127.0.0.1']
